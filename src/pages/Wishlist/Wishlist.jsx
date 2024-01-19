35ec3cfd5af66f6baf72a714762b4963e696067a
LeanneCodes <leannegoldsmith95@yahoo.com>
2024-01-19 17:42:00 +0000

    add overlay to saved sessions and saved players and move the overlay and page title css to index.css

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -9,7 +9,7 @@ const Wishlist = () => {
       <div className='overlay'></div>
 
       {/* content */}
-      <h1 className='font-main text-red text-center'>Wishlist</h1>
+      <h1 className='font-main text-red text-center mt-10 pageTitle'>Wishlist</h1>
       <p className='font-smallText text-center mt-4'>Click on the selected title to start scheduling</p>
 
       {/* wishlist cards */}
