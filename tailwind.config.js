8e8ecbbbe9f5bcf98f03b8ea029855947d2cb510
Kane Chang <kane.chang.fl@gmail.com>
2024-01-19 14:22:38 +0000

    installed tailwind dependencies for react+vite, configured tailwind config file and added tailwind directives in index.css for tailwind classes to work

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx}',
+    './src/App.jsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
