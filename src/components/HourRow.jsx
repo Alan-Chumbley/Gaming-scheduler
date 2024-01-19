1e52b77b2d1b324981b556e5d1d8a9fc63758519
Kane Chang <kane.chang.fl@gmail.com>
2024-01-19 19:22:44 +0000

    added jsx code and tailwind styling to HourRow.jsx

--- src/components/HourRow.jsx
+++ src/components/HourRow.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const HourRow = () => {
-  return (
-    <div>HourRow</div>
-  )
-}
+    return (
+        <div className="flex mx-20 pl-40 mt-4 justify-between">
+            <h2 className="font-sub">1 </h2>
+            <h2 className="font-sub">2 </h2>
+            <h2 className="font-sub">3 </h2>
+            <h2 className="font-sub">4 </h2>
+            <h2 className="font-sub">5 </h2>
+            <h2 className="font-sub">6 </h2>
+            <h2 className="font-sub">7 </h2>
+            <h2 className="font-sub">8 </h2>
+            <h2 className="font-sub">9 </h2>
+            <h2 className="font-sub">10</h2>
+            <h2 className="font-sub">11</h2>
+            <h2 className="font-sub">12</h2>
+            <h2 className="font-sub">13</h2>
+            <h2 className="font-sub">14</h2>
+            <h2 className="font-sub">15</h2>
+            <h2 className="font-sub">16</h2>
+            <h2 className="font-sub">17</h2>
+            <h2 className="font-sub">18</h2>
+            <h2 className="font-sub">19</h2>
+            <h2 className="font-sub">20</h2>
+            <h2 className="font-sub">21</h2>
+            <h2 className="font-sub">22</h2>
+            <h2 className="font-sub">23</h2>
+            <h2 className="font-sub">24</h2>
+        </div>
+    );
+};
 
-export default HourRow
+export default HourRow;
