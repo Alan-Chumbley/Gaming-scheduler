b122703fa98f5cb38652b0dde474d6836b88b1e2
Kane Chang <kane.chang.fl@gmail.com>
2024-01-19 14:41:44 +0000

    added custom font and colors to tailwind classes in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ module.exports = {
     './src/App.jsx'
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    fontFamily: {
+      'main': ['abolition', 'sans-serif'], /* example: Player 1 */
+      'mainSoft': ['abolition-soft', 'sans-serif'], 
+      'sub': ['bdr-mono', 'sans-serif'],/* example: Monday */
+      'smallText': ['din-2014', 'sans-serif'], /* example: game session scheduler */
+    },
+    extend: {
+      colors: {
+        white: '#ffffff',
+        black: '#000000',
+        cyan: '#00E2FC',
+        grey: '#BFCCD8',
+        red: '#FF304D',
+        darkGrey: '#312323',
+      }
+    },
   },
   plugins: [],
 }
