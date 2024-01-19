808654da8d0c1ff6996a1716dfa751ebe030afbd
thevisualriot <rydzkowska@gmail.com>
2024-01-19 23:37:32 +0000

    clean jsx code and add comments

--- src/pages/Home/EntryForm.jsx
+++ src/pages/Home/EntryForm.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import { useState, useEffect } from "react";
 import ActionBtn from '../../components/Buttons/ActionBtn';
 
+
 function EntryForm() {
+
+    /* ************************************** VARIABLES ********************************************** */
 
     const genres = ['Action', 'Horror', 'Open World', 'Fantasy', 'Survival', 'Warfare', 'Thriller', 'Stealth', 'Drama', 'Mystery', 'Historical', 'Sci-Fi'];
 
@@ -14,78 +17,65 @@ function EntryForm() {
 
     let genresBtns;
 
+    /* ************************************** HANDLING EVENTS *************************************** */
+
+    // INPUT (GAME TITLE AND SQUAD ALIAS)
     const handleChange = (setState) => ({ target }) => {
         setState(target.value);
     }
 
-
-
-
+    // GENRE QUEST: BUTTONS
     const handleClick = (setState) => ({ target }) => {
         const selectedBtn = target.firstChild.data;
 
         const buttons = document.querySelector('#genresBtn');
         const btnChildren = buttons.children;
 
-        for(let i=0; i < btnChildren.length; i++){
+        for (let i = 0; i < btnChildren.length; i++) {
             btnChildren[i].classList.remove('selectedBtn');
         }
 
         target.classList.add('selectedBtn');
 
         setState(selectedBtn);
-        
     }
 
-
-    useEffect(() => {
-        console.log('Updated state:', genre);
-    }, [genre]);
-
-
-
-
-
-
-
-
-
-
+    // SUBMIT (LETS GO BUTTON)
     const handleSubmit = ({ target }) => {
-        console.log(team);
-        console.log(game);
-        console.log(genre);
 
         const newTeam = [{
             teamName: team,
             game: game,
             genre: genre,
         }]
-    
-        saveToLS(newTeam);
+
+        saveToLS(newTeam); // save to local storage
     }
 
-    genresBtns = genres.map(genre => <button key={genre} type="button" className="genreBtn" onClick={handleClick(setGenre)}>{genre}</button>);
+    genresBtns = genres.map(genre => <button key={genre} type="button" className="genreBtn" onClick={handleClick(setGenre)}>{genre}</button>);  // create buttons for each genre
 
 
 
+    /* *************************************** FUNCTIONS *************************************** */
 
-    function saveToLS(object){
+    //SAVE TO LOCAL STORAGE
+    function saveToLS(object) {
         storedData.push(object);
         localStorage.setItem('Teams', JSON.stringify(storedData));
     }
 
 
 
+    /* *************************************** RENDER *************************************** */    
 
     return (
         <div id="container">
+
             <div id="squad-alias">
                 <div className="sm:block md:block lg:flex items-end inputTitle">
                     <h2 className="sm:text-3xl md:text-4xl">Squad Alias</h2>
                     <p className="italic explanation md:pl-2">name your gaming team</p>
                 </div>
-                
                 <input type='text' value={team} onChange={handleChange(setTeam)} maxLength="58" />
             </div>
 
@@ -94,7 +84,7 @@ function EntryForm() {
                     <h2 className="sm:text-3xl md:text-4xl">Game Title</h2>
                     <p className="italic explanation md:pl-2">type the game title if known</p>
                 </div>
-                <input type='text' value={game} onChange={handleChange(setGame)} maxLength="58"  />
+                <input type='text' value={game} onChange={handleChange(setGame)} maxLength="58" />
             </div>
 
             <div id="genre-quest">
@@ -105,7 +95,7 @@ function EntryForm() {
                 <ul id='genresBtn'>{genresBtns}</ul>
             </div>
 
-            <ActionBtn name="Let's go" onClick={handleSubmit} id="bigBtn" />            
+            <ActionBtn name="Let's go" onClick={handleSubmit} id="bigBtn" />
 
         </div>
     );
