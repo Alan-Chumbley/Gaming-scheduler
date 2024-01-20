49014f7c9ffd0c7fad90aa480d052fccbb600398
LeanneCodes <leannegoldsmith95@yahoo.com>
2024-01-20 15:28:25 +0000

    add hover classnames to button

--- src/components/Buttons/OutlineBtn.jsx
+++ src/components/Buttons/OutlineBtn.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const OutlineBtn = (props) => {
   return (
-    <button className='rounded-full border-cyan text-cyan px-10 border-2 bg-transparent font-sub uppercase flex justify-center mx-auto mb-8 w-fit'  onClick={props.onClick} id={props.id}>
+    <button
+      className='rounded-full border-cyan text-cyan px-10 border-2 bg-transparent font-sub uppercase flex justify-center mx-auto mb-8 w-fit hover:bg-cyan hover:text-darkGrey'
+      onClick={props.onClick}
+      id={props.id}
+    >
       {props.name}
     </button>
   )
