39bf6c69270eafef92e02b289a19c52e2a1298a5
LeanneCodes <leannegoldsmith95@yahoo.com>
2024-01-20 13:04:19 +0000

    update svg attributes to tailwind attribute names

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Logo from './assets/logo.png';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css'
@@ -9,21 +9,22 @@ const Navbar = () => {
         <div className="mx-auto max-w-7xl px-2 sm:px-6">
             <div className="relative flex h-16 items-center justify-between">
                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                    
-                    {/* Menu button */}
+                    {/* Mobile menu button */}
                     <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-                    <span className="absolute -inset-0.5"></span>
-                    <span className="sr-only">Open main menu</span>
-                    
-                    {/* Menu icon when it is closed */}
-                    <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
+                        <span className="absolute -inset-0.5"></span>
+                        <span className="sr-only">Open main menu</span>
+                            {/* Icon when menu is closed.
 
-                    {/* Menu icon when it is open */}
-                    <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
+                            Menu open: "hidden", Menu closed: "block" */}
+                        <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" aria-hidden="true">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
+                            {/* Icon when menu is open.
+
+                            Menu open: "block", Menu closed: "hidden" */}
+                        <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" aria-hidden="true">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
                     </button>
                 </div>
                 <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start md:justify-between">
