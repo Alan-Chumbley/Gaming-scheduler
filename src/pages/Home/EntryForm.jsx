b6baa47bb7139a11bb1307ab6b165907193f450c
thevisualriot <rydzkowska@gmail.com>
2024-01-20 16:05:15 +0000

    corrected saving to localstorage

--- src/pages/Home/EntryForm.jsx
+++ src/pages/Home/EntryForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import ActionBtn from '../../components/Buttons/ActionBtn';
 
 function EntryForm() {
@@ -51,7 +51,7 @@ function EntryForm() {
         const buttons = document.querySelector('#genresBtn');
         const btnChildren = buttons.children;
 
-        if(!target.classList.contains('selectedBtn')){
+        if (!target.classList.contains('selectedBtn')) {
             for (let i = 0; i < btnChildren.length; i++) {
                 btnChildren[i].classList.remove('selectedBtn');
             }
@@ -70,17 +70,19 @@ function EntryForm() {
 
         errorEl = document.querySelector('#error-msg');
 
-        if (!team && !game && !genre || !game && !genre || !team){
+        if (!team && !game && !genre || !game && !genre || !team) {
             errorEl.removeAttribute('hidden');
-        }
+        } else {
 
-        const newTeam = {
-            teamName: team,
-            game: game,
-            genre: genre,
-        }
+            const newTeam = {
+                teamName: team,
+                game: game,
+                genre: genre,
+            }
 
-        saveToLS(newTeam); // save to local storage
+            saveToLS(newTeam); // save to local storage
+
+        }
     };
 
     genresBtns = genres.map((genre) => (
@@ -120,10 +122,10 @@ function EntryForm() {
         }
     }
 
-    function toggleInput(state){
+    function toggleInput(state) {
         const gameInput = document.querySelector('#game-input');
 
-        if(state === 'disable'){
+        if (state === 'disable') {
             gameInput.setAttribute('disabled', true);
             gameInput.classList.add('disabled');
         } else {
