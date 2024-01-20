85e6dccd54bc28e4cce98cb86b990878a1b32e7e
Alan-Chumbley <alanchumbley28@gmail.com>
2024-01-20 20:28:29 +0000

    RecommendSearch export default fix

--- src/pages/Recommendation/RecommendSearch.jsx
+++ src/pages/Recommendation/RecommendSearch.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //** Authentication *//
-
+export default function RecommendSearch(){
 const clientID = "so4qo327upvikt16q9cw70yyii1gz9"; // insert your client ID
 const clientSecret = "gd9dja25tgougldunbc8r5h1rl80al"; // insert your client secret
 let AuthURL = `https://id.twitch.tv/oauth2/token?client_id=${clientID}&client_secret=${clientSecret}&grant_type=client_credentials`;
@@ -12,7 +12,7 @@ axios.post(AuthURL, "")
         const access_token = response.data.access_token;
 
         //** API calls **//
-recommended
+
         const gameURL = "https://api.igdb.com/v4/games"; // Your API call URL
         let body = 'fields *'; // Edit your API call fields
 
@@ -39,3 +39,5 @@ recommended
     .catch(function (error) {
         console.log(error);
     });
+
+}
