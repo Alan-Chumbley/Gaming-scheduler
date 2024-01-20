7c6bf5dfb08127d7d2e8f16a18fbf9891d687b05
Kane Chang <kane.chang.fl@gmail.com>
2024-01-20 13:46:59 +0000

    added comments on PlayerInputContainer.jsx

--- src/components/PlayerInputComponents/PlayerInputContainer.jsx
+++ src/components/PlayerInputComponents/PlayerInputContainer.jsx
@@ -4,22 +4,27 @@ import PlayerCalendar from "./PlayerCalendar";
 import { useState } from "react";
 
 const PlayerInputContainer = () => {
+    //** Variables **//
     const [playerName, setPlayerName] = useState();
-    
     let storedPlayers = JSON.parse(localStorage.getItem("Players")) || [];
+
+    //** Save to Local Storage Function **/
     const saveToLS = (data) => {
         storedPlayers.push(data)
         localStorage.setItem('Players', JSON.stringify(storedPlayers));
     };
 
+    //** Handler Functions **/
     const handleInputChange = (event) => {
         setPlayerName(event.target.value);
     };
 
     const handleAddPlayer = () => {
+
         const dayRowArray = ['#MondayRow', '#TuesdayRow', '#WednesdayRow', '#ThursdayRow', '#FridayRow', '#SaturdayRow', '#SundayRow']
         const playerAvailability = []
 
+        // Loops through each row and each hour to check for toggled timeslots and stores into playerAvailability Array.
         for (let j = 0; j < dayRowArray.length; j++) {
             const Row = document.querySelector(dayRowArray[j])
             for (let i = 0; i < Row.children.length; i++) {
@@ -38,6 +43,7 @@ const PlayerInputContainer = () => {
         saveToLS(playerData);
     };
 
+    //** Render components **/
     return (
         <div className="flex flex-col">
             <PlayerNameInput
