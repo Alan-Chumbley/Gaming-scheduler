0a6942a836bd3ad49f2f5da19e30e2534a6241c8
Kane Chang <kane.chang.fl@gmail.com>
2024-01-21 16:16:45 +0000

    added staticRow.jsx component to render the calendar for summary page

--- src/pages/Summary/SummaryCal.jsx
+++ src/pages/Summary/SummaryCal.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import HourRow from '../../components/PlayerInputComponents/HourRow'
-import DayRow from '../../components/PlayerInputComponents/DayRow';
+import React from "react";
+import HourRow from "../../components/PlayerInputComponents/HourRow";
+import StaticRow from "./StaticRow";
 
 const SummaryCal = () => {
     const days = [
-        "Mon",
-        "Tue",
-        "Wed",
-        "Thu",
-        "Fri",
-        "Sat",
-        "Sun",
+        { short: "Mon", long: "Monday" },
+        { short: "Tue", long: "Tuesday" },
+        { short: "Wed", long: "Wednesday" },
+        { short: "Thu", long: "Thursday" },
+        { short: "Fri", long: "Friday" },
+        { short: "Sat", long: "Saturday" },
+        { short: "Sun", long: "Sunday" },
     ];
-    const dayRows = days.map((day) => <DayRow day={day} key={day} />);
-    
 
+    const dayRows = days.map((day) => (
+        <StaticRow shortenedDay={day.short} day={day.long} key={day} />
+    ));
 
+    return (
+        <div>
+            <HourRow classList="flex mt-4 mb-4 ml-14 justify-between" />
+            {dayRows}
+        </div>
+    );
+};
 
-  return (
-    <div>
-        <HourRow classList="flex mt-4 mb-2 justify-between"/>
-        {/* {dayRows} */}
-    </div>
-  )
-}
-
-export default SummaryCal
+export default SummaryCal;
