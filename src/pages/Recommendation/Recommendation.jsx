6c32eed7a81203712f16ae0a650feebb78256bf7
LeanneCodes <leannegoldsmith95@yahoo.com>
2024-01-22 21:39:12 +0000

    parse through the genre name in the title for the recommendations page

--- src/pages/Recommendation/Recommendation.jsx
+++ src/pages/Recommendation/Recommendation.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 import GameCard from '../../components/GameCard/GameCard';
 
 const Recommendation = () => {
+  const genreTitle = localStorage.getItem('CurrentTeam');
+  const genreParsed = JSON.parse(genreTitle).genre;
+  console.log(genreParsed);
+
   return (
-    <div>Recommendation</div>
+    <div className='recommendations'>
+
+      {/* content */}
+      <h1 className='font-main text-cyan text-center mt-10 pageTitle'>Recommendations for {genreParsed} Games</h1>
+
+      
+      
+    </div>
   )
 }
 
