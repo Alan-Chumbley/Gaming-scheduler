e5146da66597f68cae68fc837be2491ef2c2996a
LeanneCodes <leannegoldsmith95@yahoo.com>
2024-01-22 22:54:20 +0000

    successfully render api data to application

--- src/pages/Recommendation/Recommendation.jsx
+++ src/pages/Recommendation/Recommendation.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import GameCard from '../../components/GameCard/GameCard';
 
 const Recommendation = () => {
+  const [genreData, setGenreData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // calls data once
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://api.rawg.io/api/genres?key=15dc7ef863d140f8b11adec2cc08a02b');
+        setGenreData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const genreTitle = localStorage.getItem('CurrentTeam');
   const genreParsed = JSON.parse(genreTitle).genre;
   console.log(genreParsed);
 
-  const genreDetails = JSON.parse(Genre);
-  console.log(genreDetails)
+  // while the data is load the below will render
+  if (loading) {
+    return <p className='font-sub text-red text-center'>Loading...</p>;
+  }
 
   return (
     <div className='recommendations'>
@@ -16,8 +37,11 @@ const Recommendation = () => {
       {/* title */}
       <h1 className='font-main text-cyan text-center mt-10 pageTitle'>Recommendations for {genreParsed} Games</h1>
 
+      {/* genre data */}
+      <p>Data: {JSON.stringify(genreData.results)}</p>
+
+
       {/* genre cards */}
-      console.log()
 
 
       
