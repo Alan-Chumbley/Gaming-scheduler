f2fcab686b129b876fcfaa136aea8588095fee25
thevisualriot <rydzkowska@gmail.com>
2024-01-22 15:39:18 +0000

    responsiveness of player input name for small screens

--- src/components/PlayerInputComponents/PlayerNameInput.jsx
+++ src/components/PlayerInputComponents/PlayerNameInput.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const PlayerNameInput = (props) => {
     return (
-        <div className='flex sm:flex-col sm:items-center sm:px-0 md:flex-row p-20'>
+        <div className='flex flex-col items-center px-0 md:flex-row p-20'>
 
-            <div className="sm:pb-10 md:pb-0">
+            <div className="pb-10 md:pb-0">
                 <h1 className="font-main text-5xl text-nowrap uppercase whitespace-nowrap">
                     {props.playerNum}
                 </h1>
             </div>
 
-            <div className="w-full ml-14 mt-1">
+            <div className="w-8/12 md:w-full md:ml-14 md:mt-1">
                 <input
                     type="text"
                     className="w-full h-100 right-0"
