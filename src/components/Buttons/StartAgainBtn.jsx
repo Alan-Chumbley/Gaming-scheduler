c3f49f829e0b563bd4085010c5b3c381e1e40fd4
thevisualriot <rydzkowska@gmail.com>
2024-01-22 21:48:57 +0000

    start again button - styling

--- src/components/Buttons/StartAgainBtn.jsx
+++ src/components/Buttons/StartAgainBtn.jsx
@@ -4,8 +4,8 @@ import { BiRefresh } from "react-icons/bi";
 
 const StartAgainBtn = (props) => {
   return (
-    <button className='rounded-full border-cyan outline-2 bg-transparent startBtn'  onClick={props.onClick} id={props.id}>
-        {<BiRefresh />}{props.name}
+    <button className='bg-transparent border-cyan border-2 rounded-full flex w-fit px-6 p-1 mr-4 uppercase text-cyan text-xl hover:text-black hover:bg-cyan disabled:opacity-40 disabled:hover:bg-transparent disabled:hover:text-cyan startBtn'  onClick={props.onClick} id={props.id}>
+        {<BiRefresh id='fa-refresh-icon' />}{props.name}
     </button>
   )
 }
