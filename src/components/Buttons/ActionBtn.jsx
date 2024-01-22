1a78ce17d4e3990b43a8128ff4356bf9f7eee5b7
thevisualriot <rydzkowska@gmail.com>
2024-01-22 13:01:03 +0000

    styling of Action Btn

--- src/components/Buttons/ActionBtn.jsx
+++ src/components/Buttons/ActionBtn.jsx
@@ -1,14 +1,32 @@
 import React from "react";
+import './Buttons.css';
+import { FaPlay } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 const ActionBtn = (props) => {
+
+    function handleHover(e) {
+        e.target.children[0].removeAttribute('hidden');
+    }
+
+    function handleHoverExit(e) {
+        if(!e.target.children[0].hasAttribute('hidden')){
+            e.target.children[0].setAttribute('hidden', true);
+        }
+    }
+
     return (
-        <button
-            className="rounded-full border-cyan outline-2 bg-cyan"
+        
+        <button onMouseEnter={handleHover} onMouseLeave={handleHoverExit}
+            className="rounded-full border-cyan outline-2 bg-cyan actionBtn"
             onClick={props.onClick}
             id={props.id}
         >
             {props.name}
+            <FaPlay hidden id='fa-play-icon' /> 
+
         </button>
+        
     );
 };
 
