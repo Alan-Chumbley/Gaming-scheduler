f18f5dc3b76035b1e527598ce60fda7a3000793b
Alan-Chumbley <alanchumbley28@gmail.com>
2024-01-22 21:21:46 +0000

    pushing with bugs and updated card

--- src/pages/Recommendation/RecommendationCard.jsx
+++ src/pages/Recommendation/RecommendationCard.jsx
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from 'react';
 import GameCard from '../../components/GameCard/GameCard';
 import './Recommendation.css'; // Import your CSS file
 import { RecommendSearch } from './RecommendSearch'; 
+import EntryForm from '../Home/EntryForm';
 
 const Recommendation = () => {
   let [gameLibrary, setgameLibrary] = useState([]);
  //nothing to run so it only commences on load -one time. 
 useEffect(() => {
-  RecommendSearch('shooter').then((data)=> {
+  RecommendSearch(EntryForm.genre).then((data)=> {
     gameLibrary = []
-    setgameLibrary(gameLibrary)
+    setgameLibrary(gameLibrary) // INPUT
     // console.log("Jung Spongebob",data.results);
     gameLibrary.push(...data.results)
     setgameLibrary(...data.results)
@@ -29,6 +30,7 @@ useEffect(() => {
             {/* refer back to gameCard and pass through props? */}
 <GameCard key ={game.slug} url={game.background_image} name={game.name} />
 
+
 })}
                 
       </div>
@@ -37,3 +39,5 @@ useEffect(() => {
 };
 
 export default Recommendation;
+
+//https://api.rawg.io/api/games/28?key=15dc7ef863d140f8b11adec2cc08a02b
