aad3824598a4d497c7b9e41175c8f28aa3561bfc
LeanneCodes <leannegoldsmith95@yahoo.com>
2024-01-22 22:06:47 +0000

    call the genre data

--- src/pages/Recommendation/Recommendation.jsx
+++ src/pages/Recommendation/Recommendation.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import GameCard from '../../components/GameCard/GameCard';
 
 const Recommendation = () => {
+
   const genreTitle = localStorage.getItem('CurrentTeam');
   const genreParsed = JSON.parse(genreTitle).genre;
   console.log(genreParsed);
@@ -9,8 +10,11 @@ const Recommendation = () => {
   return (
     <div className='recommendations'>
 
-      {/* content */}
+      {/* title */}
       <h1 className='font-main text-cyan text-center mt-10 pageTitle'>Recommendations for {genreParsed} Games</h1>
+
+      {/* genre cards */}
+
 
       
       
