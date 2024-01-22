8744a69f8c3557955d70cb1c965d67cf291b8fb6
thevisualriot <rydzkowska@gmail.com>
2024-01-22 21:40:52 +0000

    saveBtn styling

--- src/components/Buttons/SaveBtn.jsx
+++ src/components/Buttons/SaveBtn.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { FaHeart } from "react-icons/fa";
+import './Buttons.css';
 
 const SaveBtn = (props) => {
   return (
-    <button className='rounded-full border-cyan outline-2 bg-transparent'  onClick={props.onClick} id={props.id}>
-        {<FaHeart />}{props.name}
+    <button className='bg-transparent border-cyan border-2 rounded-full flex w-fit px-6 p-1 mr-4 uppercase text-cyan text-xl hover:text-black hover:bg-cyan disabled:opacity-40 disabled:hover:bg-transparent disabled:hover:text-cyan saveBtn'  onClick={props.onClick} id={props.id}>
+        {<FaHeart id='fa-heart-icon' />}{props.name}
     </button>
   )
 }
 
 export default SaveBtn
+
--- src/components/Buttons/StartAgainBtn.jsx
+++ src/components/Buttons/StartAgainBtn.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import './Buttons.css';
 import { BiRefresh } from "react-icons/bi";
 
 const StartAgainBtn = (props) => {
   return (
-    <button className='rounded-full border-cyan outline-2 bg-transparent'  onClick={props.onClick} id={props.id}>
+    <button className='rounded-full border-cyan outline-2 bg-transparent startBtn'  onClick={props.onClick} id={props.id}>
         {<BiRefresh />}{props.name}
     </button>
   )
