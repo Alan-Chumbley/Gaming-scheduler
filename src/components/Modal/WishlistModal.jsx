99ab3095a342b9ed5b972942026af646d2758622
LeanneCodes <leannegoldsmith95@yahoo.com>
2024-01-23 22:15:37 +0000

    boilerplate code from tailwind

--- src/components/Modal/WishlistModal.jsx
+++ src/components/Modal/WishlistModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const WishlistModal = () => {
+const WishlistModal = (props) => {
     const [ isWishlistModalOpen, setIsWishlistModalOpen ] = useState(false);
 
     // toggle function for modal
@@ -15,7 +15,86 @@ const WishlistModal = () => {
     };
 
     return (
-        <div>WishlistModal</div>
+        <div>
+            {/* modal toggle button */}
+            <button
+                data-modal-target="crud-modal"
+                data-modal-toggle="crud-modal"
+                className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                type="button"
+                onClick={toggleWishlistModal}
+            >
+                Toggle modal
+            </button>
+
+            {/* modal body */}
+            {isWishlistModalOpen && (
+                <div
+                    id="crud-modal"
+                    tabIndex="-1"
+                    aria-hidden="true"
+                    className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center h-screen w-full bg-black bg-opacity-40"
+                >
+                    <div className="relative p-4 w-full max-w-md">
+                        {/* Modal content */}
+                        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                            {/* Modal header */}
+                            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                                {props.name}
+                                </h3>
+                                <button
+                                type="button"
+                                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                onClick={toggleWishlistModal}
+                                >
+                                <svg
+                                    className="w-3 h-3"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 14 14"
+                                >
+                                    <path
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                                    />
+                                </svg>
+                                <span className="sr-only">Close modal</span>
+                                </button>
+                            </div>
+                            {/* Modal body */}
+                            <form className="p-4 md:p-5" onSubmit={handleSubmit}>
+                                {/* Form inputs */}
+
+                                {/* Submit button */}
+                                <button
+                                type="submit"
+                                className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                >
+                                <svg
+                                    className="me-1 -ms-1 w-5 h-5"
+                                    fill="currentColor"
+                                    viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                    fillRule="evenodd"
+                                    d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                                    clipRule="evenodd"
+                                    />
+                                </svg>
+                                Let's Schedule
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
     )
 }
 
