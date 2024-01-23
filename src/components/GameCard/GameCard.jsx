5aadfc0c39250f4f6654e445069787a5695b3426
thevisualriot <rydzkowska@gmail.com>
2024-01-23 15:24:08 +0000

    fix delayed state

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -3,13 +3,32 @@ import { TfiNewWindow } from "react-icons/tfi";
 import './GameCard.css'
 import WishlistBtn from '../Buttons/WishlistBtn';
 import MoreBtn from '../Buttons/MoreBtn';
+import { FaStar } from 'react-icons/fa';
 
 const GameCard = (props) => {
+  const [SelectedGame, setSelectedGame] = React.useState(false);
   const [isToggled, setIsToggled] = React.useState(false);
 
   function handleWishlistClick(e) {
-    setIsToggled(!isToggled);
-    e.target.classList.toggle('smallBtn-toggled', isToggled);
+    setIsToggled(prevIsToggled => {
+      const newIsToggled = !prevIsToggled;
+      console.log(newIsToggled)
+      e.target.classList.toggle('smallBtn-toggled', newIsToggled);
+      return newIsToggled;
+    });
+  }
+
+  const handleSelectClick = (e) => {
+    setSelectedGame(prevSelectedGame => {
+      const newSelectedGame = !prevSelectedGame;
+      console.log(newSelectedGame);
+      if(newSelectedGame){
+        e.target.classList.add('selected-card');
+      } else if (!newSelectedGame && e.target.hasAttribute('selected-card')) {
+        e.target.classList.remove('selected-card');
+      }
+      return newSelectedGame;
+    });
   }
 
   useEffect(() => {
@@ -20,8 +39,8 @@ const GameCard = (props) => {
     <div className='flex flex-row gameCard p-10'>
       <div className='rounded-xl relative imgHolder'>
         <img src={props.imageUrl} alt={props.name} className='gameImg' />
-        <div className='absolute inset-x-0 bottom-0 select flex justify-center items-center'>
-          <p className='font-main text-black select-text'>Select</p>
+        <div className='absolute inset-x-0 bottom-0 select flex justify-center items-center' onClick={handleSelectClick}>
+          <p className='font-main text-black select-text'> <FaStar hidden /> Select</p>
         </div>
       </div>
       <div className='flex flex-col ml-10'>
