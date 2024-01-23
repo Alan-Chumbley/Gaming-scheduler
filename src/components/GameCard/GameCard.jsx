af409006800f8ce5eb6c920259389874e37477dc
thevisualriot <rydzkowska@gmail.com>
2024-01-23 14:51:45 +0000

    wishlist button toggle effect

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-import { CiHeart } from "react-icons/ci";
 import { TfiNewWindow } from "react-icons/tfi";
 import './GameCard.css'
 import WishlistBtn from '../Buttons/WishlistBtn';
@@ -7,33 +6,29 @@ import WishlistBtn from '../Buttons/WishlistBtn';
 const GameCard = (props) => {
   const [isToggled, setIsToggled] = React.useState(false);
 
-  // useEffect(() => {
-  //   console.log(isToggled);
-  // }, [isToggled]);
-
-  function handleWishlistClick(e){
-    setIsToggled(prevState => !prevState);
-    const firstChild = e.target;
-    firstChild.classList.toggle('smallBtn-toggled', isToggled);
+  function handleWishlistClick(e) {
+    setIsToggled(!isToggled);
+    e.target.classList.toggle('smallBtn-toggled', isToggled);
   }
 
-
+  useEffect(() => {
+  }, [isToggled]);
 
 
   return (
     <div className='flex flex-row gameCard p-10'>
       <div className='rounded-xl relative imgHolder'>
-          <img src={props.imageUrl} alt={props.name} className='gameImg' />
-          <div className='absolute inset-x-0 bottom-0 select flex justify-center items-center'>
-              <p className='font-main text-black select-text'>Select</p>
-          </div>
+        <img src={props.imageUrl} alt={props.name} className='gameImg' />
+        <div className='absolute inset-x-0 bottom-0 select flex justify-center items-center'>
+          <p className='font-main text-black select-text'>Select</p>
+        </div>
       </div>
       <div className='flex flex-col ml-10'>
         <h2 className='font-sub text-cyan text-2xl'>{props.name}</h2>
         <p className='font-smallText text-white mt-4'>{props.description}</p>
         <div className='flex flex-row text-pinkHover mt-4'>
-          <WishlistBtn name="Add to Wishlist" id='wishlist-button' onClick={handleWishlistClick} /> 
-          <button><a href={props.website} target='_blank' className='font-smallText flex text-size pl-5'>{<TfiNewWindow className='icon'/>} &nbsp; Read more</a></button>
+          <WishlistBtn name="Add to Wishlist" id='wishlist-button' onClick={handleWishlistClick} />
+          <button><a href={props.website} target='_blank' className='font-smallText flex text-size pl-5'>{<TfiNewWindow className='icon' />} &nbsp; Read more</a></button>
         </div>
       </div>
     </div>
