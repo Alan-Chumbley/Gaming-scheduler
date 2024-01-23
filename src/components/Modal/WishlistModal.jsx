ea449a2fa6fb79e9acaaf6b74c3c60204ce0288d
LeanneCodes <leannegoldsmith95@yahoo.com>
2024-01-23 23:44:43 +0000

    clean up code and add usestate to dropdown.jsx

--- src/components/Modal/WishlistModal.jsx
+++ src/components/Modal/WishlistModal.jsx
@@ -3,7 +3,7 @@ import Dropdown from './Dropdown';
 import { useNavigate } from 'react-router-dom';
 
 const WishlistModal = (props) => {
-    const [ isWishlistModalOpen, setIsWishlistModalOpen ] = useState(false);
+    const [isWishlistModalOpen, setIsWishlistModalOpen] = useState(false);
 
     // toggle function for modal
     const toggleWishlistModal = () => {
@@ -15,18 +15,18 @@ const WishlistModal = (props) => {
     // handle submit function for form
     const handleSubmit = (e) => {
         e.preventDefault();
-    
-        // save the squad name, name of the game and go to player 1 page
+
+        // save the squad name, name of the game, and go to player 1 page
         const { teamName, game } = props.game || {};
-    
+
         if (teamName && game) {
             const wishTeam = {
                 teamName: teamName,
                 game: game,
             };
-    
+
             saveToLS(wishTeam); // save to local storage
-    
+
             // navigate to player 1 page
             navigate('/player1');
         }
@@ -63,29 +63,29 @@ const WishlistModal = (props) => {
                             {/* Modal header */}
                             <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                                 <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                                {props.name}
+                                    {props.gameName}
                                 </h3>
                                 <button
-                                type="button"
-                                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                                onClick={toggleWishlistModal}
+                                    type="button"
+                                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                    onClick={toggleWishlistModal}
                                 >
-                                <svg
-                                    className="w-3 h-3"
-                                    aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 14 14"
-                                >
-                                    <path
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                                    />
-                                </svg>
-                                <span className="sr-only">Close modal</span>
+                                    <svg
+                                        className="w-3 h-3"
+                                        aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 14 14"
+                                    >
+                                        <path
+                                            stroke="currentColor"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                                        />
+                                    </svg>
+                                    <span className="sr-only">Close modal</span>
                                 </button>
                             </div>
                             {/* Modal body */}
@@ -93,9 +93,9 @@ const WishlistModal = (props) => {
                                 {/* Form inputs */}
                                 {/* Game image card */}
                                 <div>
-                                    <img src={props.imageUrl} alt={props.name} className='rounded-[16px] w-100 h-100 object-cover'/>
+                                    <img src={props.imageUrl} alt={props.gameName} className='rounded-[16px] w-100 h-100 object-cover'/>
                                 </div>
-                                
+
                                 {/* Squad selection */}
                                 <div>
                                     <Dropdown />
@@ -103,22 +103,22 @@ const WishlistModal = (props) => {
 
                                 {/* Submit button */}
                                 <button
-                                type="submit"
-                                className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                    type="submit"
+                                    className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                 >
-                                <svg
-                                    className="me-1 -ms-1 w-5 h-5"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                    fillRule="evenodd"
-                                    d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                                    clipRule="evenodd"
-                                    />
-                                </svg>
-                                Let's Schedule
+                                    <svg
+                                        className="me-1 -ms-1 w-5 h-5"
+                                        fill="currentColor"
+                                        viewBox="0 0 20 20"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                    Let's Schedule
                                 </button>
                             </form>
                         </div>
@@ -129,4 +129,4 @@ const WishlistModal = (props) => {
     )
 }
 
-export default WishlistModal
+export default WishlistModal;
