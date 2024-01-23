2d25c8326d6748c390cefb6a4e95a56f84492a69
thevisualriot <rydzkowska@gmail.com>
2024-01-23 20:22:14 +0000

    navbar styling - hover colors

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -52,7 +52,7 @@ export default function Example() {
                         key={item.name}
                         to={item.to}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white font-main' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          item.current ? 'bg-gray-900 text-white font-main' : 'text-gray-300 hover:text-cyan',
                           'rounded-md px-3 py-2 text-sm font-medium font-main text-white whitespace-nowrap'
                         )}
                         aria-current={item.current ? 'page' : undefined}
