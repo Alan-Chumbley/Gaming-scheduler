ff5d70afa98af8bdaee231c75df23956daacb86d
LeanneCodes <leannegoldsmith95@yahoo.com>
2024-01-23 21:55:36 +0000

    start handleSubmit function for input form inside modal

--- src/components/Modal/WishlistModal.jsx
+++ src/components/Modal/WishlistModal.jsx
@@ -3,8 +3,15 @@ import React, { useState } from 'react';
 const WishlistModal = () => {
     const [ isWishlistModalOpen, setIsWishlistModalOpen ] = useState(false);
 
+    // toggle function for modal
     const toggleWishlistModal = () => {
         setIsWishlistModalOpen(!isWishlistModalOpen);
+    };
+
+    // handle submit function for form
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
     };
 
     return (
