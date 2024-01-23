573534cd9fbf7acc811a0aed77e20448663078ab
LeanneCodes <leannegoldsmith95@yahoo.com>
2024-01-23 22:53:00 +0000

    refactor dropdown options and map through each squad name in local storage

--- src/components/Modal/Dropdown.jsx
+++ src/components/Modal/Dropdown.jsx
@@ -8,11 +8,11 @@ const Dropdown = () => {
     }
 
     const squadNames = localStorage.getItem('Teams');
+    const squadNamesJSON = JSON.parse(squadNames);
     const [selectedTeam, setSelectedTeam] = useState(null);
 
     // retrieve squad names if available
     if (squadNames !== null) {
-    const squadNamesJSON = JSON.parse(squadNames);
     console.log(squadNamesJSON);
 
     // selected team will always be the first team in the local storage array. e.g. team[0]
@@ -21,13 +21,16 @@ const Dropdown = () => {
     console.log('No data found in local storage for key "Teams".');
     }
 
-    
+    const handleTeamSelection = (team) => {
+        setSelectedTeam(team);
+    }
 
     return (
         <Menu as="div" className="relative inline-block text-left">
         <div>
             <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-            Choose your squad
+            {/* if no team is available "Choose your squad" will appear */}
+            {selectedTeam || 'Choose your squad'}
             <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
             </Menu.Button>
         </div>
@@ -42,62 +45,26 @@ const Dropdown = () => {
             leaveTo="transform opacity-0 scale-95"
         >
             <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-            <div className="py-1">
-                <Menu.Item>
-                {({ active }) => (
-                    <a
-                    href="#"
-                    className={classNames(
-                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                        'block px-4 py-2 text-sm'
-                    )}
-                    >
-                    Account settings
-                    </a>
-                )}
-                </Menu.Item>
-                <Menu.Item>
-                {({ active }) => (
-                    <a
-                    href="#"
-                    className={classNames(
-                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                        'block px-4 py-2 text-sm'
-                    )}
-                    >
-                    Support
-                    </a>
-                )}
-                </Menu.Item>
-                <Menu.Item>
-                {({ active }) => (
-                    <a
-                    href="#"
-                    className={classNames(
-                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                        'block px-4 py-2 text-sm'
-                    )}
-                    >
-                    License
-                    </a>
-                )}
-                </Menu.Item>
-                <form method="POST" action="#">
-                <Menu.Item>
-                    {({ active }) => (
-                    <button
-                        type="submit"
-                        className={classNames(
-                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                        'block w-full px-4 py-2 text-left text-sm'
-                        )}
-                    >
-                        Sign out
-                    </button>
-                    )}
-                </Menu.Item>
-                </form>
-            </div>
+                <div className="py-1">
+                    {squadNamesJSON !== null &&
+                        squadNamesJSON.map((team) => (
+                            <Menu.Item key={team}>
+                            {({ active }) => (
+                                <a
+                                href="#"
+                                className={classNames(
+                                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                    'block px-4 py-2 text-sm'
+                                )}
+                                onClick={() => handleTeamSelection(team)}
+                                >
+                                {team}
+                                </a>
+                            )}
+                            </Menu.Item>
+                        ))
+                    }
+                </div>
             </Menu.Items>
         </Transition>
         </Menu>
