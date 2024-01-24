5adf80713e7f3f67979b3303c51fa6b9641a5bda
LeanneCodes <leannegoldsmith95@yahoo.com>
2024-01-24 19:55:28 +0000

    update wishlist code and functionality

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -13,6 +13,18 @@ const Wishlist = () => {
   const [selectedGame, setSelectedGame] = useState(null);
   const [selectedSquad, setSelectedSquad] = useState('');
   const [availableSquads, setAvailableSquads] = useState([]);
+  const [currentTeam, setCurrentTeam] = useState('');
+
+  // retrieve data from local storage
+  const storedTeam = JSON.parse(localStorage.getItem('CurrentTeam'));
+
+  // set initial state for currentTeam
+  useEffect(() => {
+    if (storedTeam && storedTeam.teamName) {
+      const initialCurrentTeam = storedTeam.teamName.toUpperCase();
+      setCurrentTeam(initialCurrentTeam);
+    }
+  }, [storedTeam]);
 
   // open and close the modal functionality
   const openModal = (game) => {
@@ -21,7 +33,9 @@ const Wishlist = () => {
 
     // when the modal opens, set the squad/team names from local storage
     const squads = JSON.parse(localStorage.getItem('Teams')) || [];
-    setAvailableSquads(squads);
+    const squadNames = squads.map(squad => squad.teamName);
+    console.log(squadNames);
+    setAvailableSquads(squadNames);
   };
 
   const closeModal = () => {
@@ -30,14 +44,43 @@ const Wishlist = () => {
   };
 
   const schedule = () => {
+    // add squad name and game name to local storage
+    const newTeam = {
+      teamName: currentTeam,
+      game: selectedGame.name.split(" ").join("-"),
+      genre: '',
+    }
+
+    saveToLS(newTeam); // save to local storage
+
+    // direct user to player 1 page
     navigate('/player1');
+  }
+
+  function saveToLS(object) {
+    localStorage.setItem("CurrentTeam", JSON.stringify(object)); // Kane: I added this to keep track of the current team
   }
 
   // if the user's squad names list changes, the list within the modal will also update
   useEffect(() => {
     const squads = JSON.parse(localStorage.getItem('Teams')) || [];
-    setAvailableSquads(squads);
+    const squadNames = squads.map(squad => squad.teamName);
+    console.log(squadNames);
+    setAvailableSquads(squadNames);
   }, [modalOpen]);
+
+  // handle squad selection
+  const handleSquadSelection = (selectedSquad) => {
+    setSelectedSquad(selectedSquad);
+    const updatedCurrentTeam = selectedSquad.toUpperCase();
+    setCurrentTeam(updatedCurrentTeam);
+  };
+
+  // updates currentTeam when selectedSquad changes
+  useEffect(() => {
+    const updatedCurrentTeam = selectedSquad.toUpperCase();
+    setCurrentTeam(updatedCurrentTeam);
+  }, [selectedSquad]);
 
   const gameCards = storedGames.map((game)=>(
     <div className='w-52 mx-5 relative cursor-pointer' key={game.name} onClick={() => openModal(game)}>
@@ -72,16 +115,40 @@ const Wishlist = () => {
           <div className='fixed inset-0 bg-black opacity-50'></div>
           {/* Modal */}
           <div className='fixed inset-0 flex items-center justify-center z-50'>
-            <div className='bg-white p-8 rounded-lg relative w-96 h-96'>
+            <div className='bg-white p-8 rounded-lg relative w-96 min-h-80'>
+              
               {/* modal close button */}
               <button onClick={closeModal} className="absolute top-0 right-3 m-4 text-gray-600 hover:text-gray-800 text-4xl z-10">
                 &times;
               </button>
               <h1 className='text-3xl mb-4 font-sub text-black uppercase'>Ready to play:<br /><span className='text-red'>{selectedGame.name}</span></h1>
+              
               {/* modal body */}
-              <h2 className='font-smallText text-black'>Choose your squad:</h2>
+              <h2 className='font-smallText text-black'>Your current squad <span className='text-black'>{storedTeam ? 'is ' + currentTeam : 'has not been chosen yet'}</span></h2>
+              <hr className='my-5'/>
+              <h3>If you don't have a squad name or want to play with a different squad, choose an option from below:</h3>
+              
               {/* dropdown of available squad names from local storage */}
-              <p>You've chosen to play {selectedGame.name} with this squad. If you're happy with your choice, click "Schedule game".</p>
+              <select
+                value={selectedSquad}
+                onChange={(e) => handleSquadSelection(e.target.value)}
+                className='border rounded-md px-2 py-1 w-full text-black uppercase mt-3'
+              >
+                <option value='' disabled>Select squad</option>
+                {availableSquads.map((squad, index) => (
+                  <option key={index} value={squad}>
+                    {squad}
+                  </option>
+                ))}
+                {/* add the original squad name to the default option */}
+                {storedTeam && (
+                  <option value={storedTeam.teamName}>
+                    {storedTeam.teamName}
+                  </option>
+                )}
+              </select>
+              <hr className='my-5'/>
+              <p className='mb-10'>You've chosen to play {selectedGame?.name} with {currentTeam}. If you're happy with your choice, click "Schedule game".</p>
               <button onClick={schedule} className='bg-blue uppercase text-white font-sub px-4 py-2 rounded-md absolute bottom-6 right-6'>
                 Schedule game
               </button>
