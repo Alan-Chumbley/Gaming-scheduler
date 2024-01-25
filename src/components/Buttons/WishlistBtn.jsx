c70128b4994f7df423033202a3ad4cac48a5349b
Kane Chang <kane.chang.fl@gmail.com>
2024-01-25 00:42:40 +0000

    added slug to local storage to ensure image loads correctly every time and separated currentTeam.name and currentTeam.slug to simplify formatting code

--- src/components/Buttons/WishlistBtn.jsx
+++ src/components/Buttons/WishlistBtn.jsx
@@ -6,7 +6,7 @@ import { FaRegHeart } from "react-icons/fa";
 const WishlistBtn = (props) => {
 
     return (
-        <button className='flex text-base whitespace-nowrap wishlistBtn' onClick={props.onClick} id={props.id} data-game-url={props.dataUrl} data-game-name={props.dataName}>
+        <button className='flex text-base whitespace-nowrap wishlistBtn' onClick={props.onClick} id={props.id} data-game-url={props.dataUrl} data-game-name={props.dataName} data-game-slug={props.dataSlug}>
             <FaHeart id='fa-heart-solid-icon' /> <FaRegHeart id='fa-heart-outline-icon' />{props.name}
         </button>
     )
