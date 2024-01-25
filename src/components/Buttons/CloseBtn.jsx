6edebd6ab09eccfaacf8b3c8efcfbe0375d21e0a
thevisualriot <rydzkowska@gmail.com>
2024-01-25 15:23:52 +0000

    fixed styling of buttons on summary

--- src/components/Buttons/CloseBtn.jsx
+++ src/components/Buttons/CloseBtn.jsx
@@ -6,7 +6,7 @@ const CloseBtn = (props) => {
 
     return (
         <button className='text-2xl md:text-3xl z-10 close-btn' onClick={props.onClick} id={props.id}>
-           <FaTimes id='fa-times-icon' /> {props.name}
+            <FaTimes id='fa-times-icon' /> {props.name}
         </button>
     )
 }
